bfde83d6b58702fffd557d963a34cbfef715b5a3
Tiago Siebler <tiagosiebler@github.com>
2018-01-22 13:07:32 +0000

    Tweak ESLint settings and update package.json.

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  "env": {
+    "es6": true,
+    "node": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "sourceType": "module"
+  },
+  "rules": {
+    "array-bracket-spacing": ["error", "never"],
+    "indent": [
+      "warn",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ],
+    "new-cap": 1,
+    "no-console": "off",
+    "no-mixed-spaces-and-tabs": 2,
+    "no-use-before-define": [2, "nofunc"],
+    "no-unreachable": ["warn"],
+    "quotes": [2, "single", "avoid-escape"],
+    "block-scoped-var": 2,
+    "brace-style": [2, "1tbs", { "allowSingleLine": true }],
+    "computed-property-spacing": [2, "never"],
+    "keyword-spacing": 2,
+    "space-unary-ops": 2
+
+
+  }
+};
